0e8e8b03f5e93a52674e906c1868efb583faf9cb
Adam Sisk <adamsisk91@gmail.com>
2018-12-28 12:16:00 -0600

    backend mutation and schema have signup functionality

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,6 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const Mutations = {
   async createItem(parent, args, ctx, info) {
     //* TODO: Check if they are logged in
@@ -37,6 +40,32 @@ const Mutations = {
     // TODO
     // 3. Delete it!
     return ctx.db.mutation.deleteItem({ where }, info);
+  },
+  async signup(parent, args, ctx, info) {
+    // lowercase their email to avoid problems in the future i.e. logging in
+    args.email = args.email.toLowerCase();
+    // hash their password
+    const password = await bcrypt.hash(args.password, 10);
+    // create the user in the database
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ['USER'] },
+        },
+      },
+      info,
+    );
+    // create the JWT (JSON Web Token) for them
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
+    // we set the JWT as a cookie on the response
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+    });
+    // finallllly we return the user to the browser
+    return user;
   },
 };
 
