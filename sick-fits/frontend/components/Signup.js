786b6fdb4daa2d6ba76886a13d8344d9cc753bd6
Adam Sisk <adamsisk91@gmail.com>
2018-12-28 19:00:19 -0600

    sqaushed bug: not destructuring imported query

--- sick-fits/frontend/components/Signup.js
+++ sick-fits/frontend/components/Signup.js
@@ -3,7 +3,7 @@ import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 import Form from './styles/Form';
 import Error from './ErrorMessage';
-import CURRENT_USER_QUERY from './User';
+import { CURRENT_USER_QUERY } from './User';
 
 const SIGNUP_MUTATION = gql`
   mutation SIGNUP_MUTATION(
