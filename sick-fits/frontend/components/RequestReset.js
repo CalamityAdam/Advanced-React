99dc03db1b74b0413e2ec9cc8f0acd0fd6e1a7c0
Adam Sisk <adamsisk91@gmail.com>
2019-01-02 22:18:24 -0600

    request reset testing

--- sick-fits/frontend/components/RequestReset.js
+++ sick-fits/frontend/components/RequestReset.js
@@ -3,7 +3,6 @@ import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 import Form from './styles/Form';
 import Error from './ErrorMessage';
-import { CURRENT_USER_QUERY } from './User';
 
 const REQUEST_RESET_MUTATION = gql`
   mutation REQUEST_RESET_MUTATION($email: String!) {
@@ -26,6 +25,7 @@ class RequestReset extends Component {
       <Mutation mutation={REQUEST_RESET_MUTATION} variables={this.state}>
         {(reset, { error, loading, called }) => (
           <Form
+            data-test="form"
             method="post"
             onSubmit={async e => {
               e.preventDefault();
@@ -59,3 +59,4 @@ class RequestReset extends Component {
 }
 
 export default RequestReset;
+export { REQUEST_RESET_MUTATION };
